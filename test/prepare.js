03321a91afcbfba282e9a4279b8f3ec326617520
André Cruz <andremiguelcruz@msn.com>
2015-07-01 22:52:37 +0100

    Fix tests.

--- test/prepare.js
+++ test/prepare.js
@@ -8,6 +8,7 @@ glob.sync('prepare_*', { cwd: __dirname + '/fixtures' }).forEach(function (file)
     var finalFile = file.replace(/^prepare_/, '').replace(/\.sh$/, '');
 
     fs.writeFileSync(fixturesDir + '/' + finalFile, contents);
+    fs.chmodSync(fixturesDir + '/' + finalFile, 0777);
 
     process.stdout.write('Copied "' + file + '" to "' + finalFile + '"\n');
 });
