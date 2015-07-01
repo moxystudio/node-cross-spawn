47d8dee8496fa137444611f54edde2236430117e
André Cruz <andremiguelcruz@msn.com>
2015-07-01 22:45:29 +0100

    Move instead of rename.

--- test/prepare.js
+++ test/prepare.js
@@ -4,9 +4,10 @@ var fs   = require('fs');
 var fixturesDir = __dirname + '/fixtures';
 
 glob.sync('prepare_*', { cwd: __dirname + '/fixtures' }).forEach(function (file) {
+    var contents = fs.readFileSync(fixturesDir + '/' + file);
     var finalFile = file.replace(/^prepare_/, '').replace(/\.sh$/, '');
 
-    fs.renameSync(fixturesDir + '/' + file, fixturesDir + '/' + finalFile);
+    fs.writeFileSync(fixturesDir + '/' + finalFile, contents);
 
-    process.stdout.write('Moved "' + file + '" to "' + finalFile + '"\n');
+    process.stdout.write('Copied "' + file + '" to "' + finalFile + '"\n');
 });
